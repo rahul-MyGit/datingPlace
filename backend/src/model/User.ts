4365aed609714f35f1e3da7b3e3be64edfa443ae
Rahul <rahul.mymail1@gmail.com>
2024-10-31 16:24:25 +0530

    Add: users schema

--- backend/src/model/User.ts
+++ backend/src/model/User.ts
@@ -1,5 +1,64 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['male', 'female']
+    },
+    genderPreference: {
+        types: String,
+        required: true,
+        enum: ['male', 'female', 'both']
+    },
+    bio: {
+        type: String,
+        default: ""
+    },
+    image: {
+        type:String,
+        default: ""
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    dislikes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    matches: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+})
 
+userSchema.pre('save', async function (next) {
+this.password = await bcrypt.hash(this.password, 10);
+})
+7
+userSchema.methods.matchPassword = async function (enteredPassword: any) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
+const User = mongoose.model("User", userSchema);
+
+export default User;
