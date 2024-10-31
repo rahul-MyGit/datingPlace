6718ab701263f5c8149f67915cea82060878cb7f
Rahul <rahul.mymail1@gmail.com>
2024-10-31 07:32:13 +0530

    Add: Ts in BE and react init

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req: Request,res: Response)=>{
+    res.status(200).send("Hello World");
+})
+
+app.listen(3000)
