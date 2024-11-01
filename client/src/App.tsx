0781a808f1403f76cf5bd670ba912dd5ff258ea7
Rahul <rahul.mymail1@gmail.com>
2024-11-01 16:29:23 +0530

    Add: /auth route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,8 +11,9 @@ import ChatPage from "./pages/ChatPage"
 function App() {
   return (
     <>
-    <div className="absolute insert-0 -z-10 h-full w-fill bg-white bg-
-    [linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+    <div className="absolute insert-0 -z-10 h-full w-full bg-white bg-
+    [linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient
+    (to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
 
       <Routes>
         <Route path="/" element={<HomePage />}/>
