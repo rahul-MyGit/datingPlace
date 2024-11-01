55ea2620939d089d61b46fec90c852a98c543a45
Rahul <rahul.mymail1@gmail.com>
2024-11-01 16:13:01 +0530

    Add: Tailwind and Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+//TODO: remember to install react-router-dom, react-hot-toast, axios, zustand
+
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import AuthPage from "./pages/AuthPage"
+import ProfilePage from "./pages/ProfilePage"
+import ChatPage from "./pages/ChatPage"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="absolute insert-0 -z-10 h-full w-fill bg-white bg-
+    [linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/auth" element={<AuthPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+        <Route path="/chat/:id" element={<ChatPage />}/>
+      </Routes>
+    </div>
+
     </>
   )
 }
