cac7b312a13eabc3bd214f5f35e92f966b27521c
Rahul <rahul.mymail1@gmail.com>
2024-11-01 11:01:02 +0530

    Add: All routes for match

---
+++ backend/src/controller/matchController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const swipeLeft = async (req: Request, res: Response) => {
+
+}
+
+export const swipeRight = async (req: Request, res: Response) => {
+
+}
+
+export const getMatches = async (req: Request, res: Response) => {
+
+}
+
+export const getUserProfiles = async (req: Request, res: Response) => {
+
+}
--- backend/src/routes/matchRoutes.ts
+++ backend/src/routes/matchRoutes.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth";
+import { getMatches, getUserProfiles, swipeLeft, swipeRight } from "../controller/matchController";
 
 const router = express.Router();
+
+router.post('/swipe-right/:likeUserId', protectRoute, swipeRight);
+router.post('/swipe-left/:likeUserId', protectRoute, swipeLeft);
+
+router.get('/', protectRoute, getMatches)
+router.get('/user-profiles', protectRoute, getUserProfiles)
 
 export default router;
